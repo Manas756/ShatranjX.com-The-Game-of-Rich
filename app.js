2197e4b4c459e18947eeef4c5cd2645010e3b2a8
Manas <manu4f4@gmail.com>
2026-05-26 09:54:14 +0530

    fix deploy

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ io.on("connection",function(usocket){
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
